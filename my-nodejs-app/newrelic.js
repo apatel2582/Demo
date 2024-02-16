fb1cc2206143fe1dcd5c8a6a2f3bf778b8d1540f
Anishkumar Patel <chargaoshou@gmail.com>
2024-02-16 14:00:50 -0500

    Initial Commit

---
+++ my-nodejs-app/newrelic.js
@@ -0,0 +1,12 @@
+// newrelic.js
+"use strict";
+exports.config = {
+  app_name: ["My NodeJS App"],
+  license_key: "0fef981435df81a2b13741e6c61128f4FFFFNRAL",
+  logging: {
+    level: "info",
+  },
+};
+distributed_tracing: {
+  enabled: true;
+}
